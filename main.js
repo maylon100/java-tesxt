e383865cf8f8fe2d16165b57c7b9cb1ac2d013cc
mr: hetero top <149608626+maylon100@users.noreply.github.com>
2024-04-11 16:44:55 -0300

    u

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ const DatAtual = new Date();
 let dataniver = prompt ("Data do próximo niver:");
 dataniver = new Date (dataNiver+"t23:59:59")
 let diasQuefalta=Math.floor((dataNiver-dataAtual)/86400000)
+document.quruySelector("#dias_restantes").textcontent=diasQueFaltam;
