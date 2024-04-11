f4a80ba1dea94594b84adc89ea83299f3f167749
mr: hetero top <149608626+maylon100@users.noreply.github.com>
2024-04-11 16:46:51 -0300

    er

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const DatAtual = new Date();
+const DatAtual = new Date(11-04-2024);
 let dataniver = prompt ("Data do próximo niver:");
 dataniver = new Date (dataNiver+"t23:59:59")
 let diasQuefalta=Math.floor((dataNiver-dataAtual)/86400000)
