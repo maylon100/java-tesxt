877597376f67b56e3db123464ca5566d1ac12865
mr: hetero top <149608626+maylon100@users.noreply.github.com>
2024-04-24 16:20:38 -0300

    o

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const DataAtual = new Date();
 let DataObjetivo = prompt("Data Escolhida:");
 DataObjetivo = new Date(DataObjetivo + "T23:59:59");
-let diasQueFaltam = Math.floor((DataObjetivo-DataAtual)/8640000);
+let diasQueFaltam = Math.floor((DataObjetivo-DataAtual)/86400000);
 document.querySelector("#dias_restantes").textContent=diasQueFaltam;
